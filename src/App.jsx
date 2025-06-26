aac72feaeb4c10600fb07952c6c09cad23d908c9
mirakour <mirandakourt@gmail.com>
2025-06-26 13:11:52 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Account from "./pages/Account";
-import Books from "./pages/Books";
-import BookDetails from "./pages/BookDetails";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/books" />} />
-        <Route path="/books" element={<Books />} />
-        <Route path="/books/:id" element={<BookDetails />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Outlet />
     </>
   );
 }
