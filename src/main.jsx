948943f9d1bf9aeb6d178790ad99b376cba2259c
mirakour <mirandakourt@gmail.com>
2025-06-26 13:12:48 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import Books from "./pages/Books.jsx";
+import BookDetails from "./pages/BookDetails.jsx";
+import Account from "./pages/Account.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />}>
+        <Route index element={<Books />} />
+        <Route path="books" element={<Books />} />
+        <Route path="books/:id" element={<BookDetails />} />
+        <Route
+          path="account"
+          element={
+            <ProtectedRoute>
+              <Account />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
